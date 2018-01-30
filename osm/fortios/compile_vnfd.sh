3f695dafbf141f867c1d2b65c71b3b99226d269c
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-01-30 12:49:43 +0100

    VNFD script compiles charms / rename of vnfd

--- osm/fortios/compile_vnfd.sh
+++ osm/fortios/compile_vnfd.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+
+NAME="fortios_vnfd"
+
 if ! [ -d "devops" ]; then
   echo "Devops dir not present, cloning...."
   git clone https://osm.etsi.org/gerrit/osm/devops
 fi
 
-./devops/descriptor-packages/tools/generate_descriptor_pkg.sh -t vnfd -N fortios_vnfd_src
+rm -f ${NAME}.tar.gz
+
+cd ${NAME}/charms/fortios
+
+rm -rf builds/ deps/
+
+charm build
+
+if [ $? -ne 0 ]; then
+  echo "Charm is not compiling... aborting."
+  exit -1
+fi
+
+cd ${DIR}
+
+./devops/descriptor-packages/tools/generate_descriptor_pkg.sh -t vnfd -N ${NAME}
