f0f46d4272f5e0b54d288e0fe14d75e93079e1f1
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-02-02 14:35:59 +0100

    fortios_nsd moved

--- osm/fortios/compile_nsd.sh
+++ osm/fortios/compile_nsd.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+
+NAME="fortios_nsd"
+
 if ! [ -d "devops" ]; then
   echo "Devops dir not present, cloning...."
   git clone https://osm.etsi.org/gerrit/osm/devops
 fi
 
-./devops/descriptor-packages/tools/generate_descriptor_pkg.sh -t nsd -N fortigate2net_nsd
+rm -f ${NAME}.tar.gz
+
+./devops/descriptor-packages/tools/generate_descriptor_pkg.sh -t nsd -N ${NAME}
